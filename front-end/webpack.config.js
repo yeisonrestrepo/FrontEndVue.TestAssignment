71b8ae34ea098927d8f44d4306d4743a73b237ec
Frej Raahede Nielsen <frejraahede@gmail.com>
2019-09-12 09:16:33 +0200

    Cleanup

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -11,8 +11,6 @@ module.exports = {
   output: {
     filename: '[name].js',
     chunkFilename: '[name].js', // enable multiple js output files
-    // publicPath: ASSET_PATH,
-    // path: path.resolve(__dirname, 'public/assets')
   },
   plugins: [
     // Make sure the compiled styles are output as separate files
@@ -25,36 +23,14 @@ module.exports = {
       {
         test: /\.(sass|scss|css)$/,
         use: ExtractTextPlugin.extract({
-          // fallback: 'style-loader',
           use: [
             {
               loader: 'css-loader',
-              options: {
-                // If you are having trouble with urls not resolving add this setting.
-                // See https://github.com/webpack-contrib/css-loader#url
-                // url: false,
-                // minimize: true,
-                // sourceMap: false
-              }
+              options: {}
             }
           ]
         })
-      },
-      // {
-      //   test: /\.(sass|scss|css)$/,
-      //   use: [
-      //     {
-      //       loader: 'css-loader',
-      //       options: {
-      //         // If you are having trouble with urls not resolving add this setting.
-      //         // See https://github.com/webpack-contrib/css-loader#url
-      //         // url: false,
-      //         // minimize: true,
-      //         // sourceMap: false
-      //       }
-      //     }
-      //   ]
-      // }
+      }
     ]
   }
 };
