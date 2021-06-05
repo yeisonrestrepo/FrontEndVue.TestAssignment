146898eb098acca1919662675f6b044e221ef5fc
Yeison <yeison.restrepo.r@gmail.com>
2021-06-05 12:04:33 -0500

    Normalize browser styles, include Roboto font & Intro section

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -23,12 +23,7 @@ module.exports = {
       {
         test: /\.(sass|scss|css)$/,
         use: ExtractTextPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {}
-            }
-          ]
+          use: ['css-loader', 'sass-loader']
         })
       }
     ]
